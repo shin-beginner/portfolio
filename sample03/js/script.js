76d4930fa65ede5c23153f64761ae888a95853ca
shin-beginner <69285647+shin-beginner@users.noreply.github.com>
2021-12-12 17:29:05 +0900

    Add files via upload

---
+++ sample03/js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+      //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+    $('#js-page-top').click(function() {
+        $('html, body').animate( {
+            'scrollTop': 0 }, 500);
+    });
+});
